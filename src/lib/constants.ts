51a32b14863c2a6981b8f7c786b5c5df9ab42aee
himanshusxoxo <himanshusxoxo@gmail.com>
2026-02-14 01:15:06 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,7 +9,7 @@ export const loaderAnimation = [
 export const LINKS = {
   github: 'https://github.com/SandeeepKrish',
   linkedin: 'https://www.linkedin.com/in/sandeep-yadav-92a27b294/',
-  mail: 'yadavsandeep0718@gmail.com',
+  mail: 'himanshusxoxo@gmail.com',
 
 }
 
@@ -18,7 +18,7 @@ export const SITE: Site = {
   TITLE: 'Astro Sphere',
   DESCRIPTION:
     'Welcome to Astro Sphere, a portfolio and blog for designers and developers.',
-  AUTHOR: 'Sandeep Yadav',
+  AUTHOR: 'Himanshu Sharma',
 }
 
 // Work Page
@@ -46,29 +46,29 @@ export const SEARCH: Page = {
 }
 
 // Study Page
-export const STUDIES = [
+// export const STUDIES = [
+//   {
+//     title: 'Computer Science',
+//     institution: 'MCA : Model Institute of Engineering and Technology',
+//     link: 'https://www.mycamu.co.in/#/home/feed/profile',
+//     date: '2022 - 2024',
+//   },
   {
     title: 'Computer Science',
-    institution: 'MCA : Model Institute of Engineering and Technology',
-    link: 'https://www.mycamu.co.in/#/home/feed/profile',
-    date: '2022 - 2024',
-  },
-  {
-    title: '...',
     institution: 'BCA : Cluster University of Jammu',
-    link: '',
+    link: 'https://clujammu.ac.in/v1/',
     date: '2019 - 2022',
   },
-  {
-    title: '...',
-    institution: 'Coursera',
-    link: 'https://www.coursera.org/account-profile',
-    date: '2018 - 2022',
-  },
+  // {
+  //   title: '...',
+  //   institution: 'Coursera',
+  //   link: 'https://www.coursera.org/account-profile',
+  //   date: '2018 - 2022',
+  // },
    {
     title: 'XII',
-    institution: 'XII : Shiksha Niketan',
-    link: 'https://shikshaniketanjk.in/',
+    institution: 'XII : Sri Ranbir Higher Secondary School',
+    link: 'https://www.srmlschool.com/',
     date: '2017 - 2019',
   }
 
@@ -77,18 +77,18 @@ export const STUDIES = [
 
 export const EXPERIENCE = [
   {
-    company: 'TWENTYFIVE TECHNOARTS',
-    location: 'Jammu, India',
-    position: 'Software Development Engineer',
-    start: '2024 - 2025',
-    link: 'https://www.facebook.com/25Technoarts/',
+    company: 'Richa Global Exports Private Limited',
+    location: 'Gurgaon, India',
+    position: 'Java Full Stack Developer',
+    start: '2025 - Present',
+    link: 'https://www.richaglobal.com/',
 
     tasks: [
-      'Designed and implemented a hospital management system to fully digitalize operations, replacing paper-based workflows and integrating AI to assist intern doctors with real-time clinical insights.',
-      'Contributed to SkincareConnect, a live AI-driven product, by enhancing the frontend architecture, developing new responsive modules, and deploying production-ready builds via XPanel.',
-      'Engineered and optimized RESTful APIs to streamline communication between frontend, backend, and AI microservices; implemented secure authentication (JWT), request validation, pagination, and versioning with detailed API documentation using Swagger/Postman.',
-      'Architected and maintained robust databases using MongoDB, designing scalable data models, efficient indexing, and aggregation pipelines to support AI analytics and real-time data processing.',
-      'Integrated AI services within a modern frontend ecosystem built on React and Tailwind CSS, delivering a seamless, responsive user experience and enabling smooth interaction with machine learning components through well-structured REST endpoints.'
+      'Working as a full-stack developer on R-Connect, an enterprise-level application built using J-Hipster, Spring Boot, Angular and Java.',
+      'Responsible for developing and maintaining RESTful APIs, implementing complex business logic using Java, JPA and Hibernate.',
+      'Collaborate closely with cross functional team to deliver scalable, high performance solutions aligned with business requirements and also added new functionality and features to the project.',
+      'Designed and integrated a dynamic reporting engine using Jasper Reports, enabling real-time generation of mission-critical business documents (PDF/Excel) with complex Data Structure.',
+      'Designed and enhanced dynamic UI components with Angular CLI, ensuring seamless user interaction and performance.'
 
     ],
   },
